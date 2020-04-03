bac491e09bd8b374539198cad484e7451da8e9b3
Simon <wsj_simon@163.com>
2020-04-03 12:05:56 +0800

    feat: 新增插入排序

---
+++ algorithms/insertionSort.js
@@ -0,0 +1,27 @@
+/**
+ * 插入排序
+ */
+function insertionSort (a) {
+  const length = a.length
+
+  if (length <= 1) return
+
+  // 右侧未排序区域
+  for (let i = 1; i < length; i++) {
+    const value = a[i]
+    let j = i - 1
+
+    // 左侧排序区域
+    for (; j >= 0; j--) {
+      // 向右侧移动
+      if (a[j] > value) {
+        a[j+1] = a[j]
+      } else {
+        break
+      }
+    }
+
+    // 移动完后，这个位置是空的，插入
+    a[j+1] = value
+  }
+}
