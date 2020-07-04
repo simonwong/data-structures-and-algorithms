cd563a2a94caa77af070e28d7a5421560a20cf44
Simon <wsj_simon@163.com>
2020-07-04 10:41:05 +0800

    update bsearch

--- algorithms/bsearch/3.duplicateValues.js
+++ algorithms/bsearch/3.duplicateValues.js
@@ -11,16 +11,38 @@ function bsearch(a, value) {
   while (low <= high) {
     const mid = low + ((high - low) >> 1)
 
-    if (a[mid] >= value) {
+    if (a[mid] > value) {
       high = mid - 1
-    } else {
+    } else if (a[mid] < value) {
       low = mid + 1
+    } else {
+      if (mid === 0 || a[mid - 1] !== value) {
+        return mid
+      }
+      high = mid - 1
     }
   }
-
-  if (low < value.length && a[low] === value) {
-    return low
-  } else {
-    return -1
-  }
+  return -1
 }
+
+// function bsearch(a, value) {
+//   let low = 0
+//   let high = a.length - 1
+
+
+//   while (low <= high) {
+//     const mid = low + ((high - low) >> 1)
+
+//     if (a[mid] >= value) {
+//       high = mid - 1
+//     } else {
+//       low = mid + 1
+//     }
+//   }
+
+//   if (low < value.length && a[low] === value) {
+//     return low
+//   } else {
+//     return -1
+//   }
+// }
